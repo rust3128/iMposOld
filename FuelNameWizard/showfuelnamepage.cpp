5accdf28dd6b17e9a55f632aca432cb46ff2e214
rust3128 <rust3128@gmail.com>
2019-06-04 17:12:55 +0300

    1111

--- FuelNameWizard/showfuelnamepage.cpp
+++ FuelNameWizard/showfuelnamepage.cpp
@@ -47,32 +47,54 @@ void ShowFuelNamePage::getConnectionsInfo()
             listConnections.append(list);
 
     }
-    qInfo(logInfo()) << listConnections;
+//    qInfo(logInfo()) << listConnections;
 }
 
 
 void ShowFuelNamePage::createUI()
 {
     this->setTitle("<html><head/><body><p><span style='font-size:18pt; font-weight:600;color:blue'>Наименования топлива на АЗС.</span></p></body></html>");
+    ui->groupBoxGetFuelName->hide();
+
 }
 
 
 
 void ShowFuelNamePage::initializePage()
 {
-    ui->tableWidget->setColumnCount(2);
-    ui->tableWidget->horizontalHeader()->hide();
-    ui->tableWidget->verticalHeader()->hide();
-    getConnectionsInfo();
-    static int rowCount = m_listTerm.size();
-    for(int i=0; i<rowCount; ++i){
-        ui->tableWidget->insertRow(i);
-        ui->tableWidget->setItem(i,0, new QTableWidgetItem(m_listTerm.at(i)));
-        ui->tableWidget->setItem(i,1, new QTableWidgetItem("Ждем..."));
-//        ui->tableWidget->setItemDelegateForColumn(1, new ProgressBarDelegate);
-        fuelNameList(listConnections.at(i));
+    QApplication::processEvents();
+//    ui->tableWidget->setColumnCount(2);
+//    ui->tableWidget->horizontalHeader()->hide();
+//    ui->tableWidget->verticalHeader()->hide();
+//    getConnectionsInfo();
+//    static int rowCount = m_listTerm.size();
+//    for(int i=0; i<rowCount; ++i){
+//        ui->tableWidget->insertRow(i);
+//        ui->tableWidget->setItem(i,0, new QTableWidgetItem(m_listTerm.at(i)));
+//        ui->tableWidget->setItem(i,1, new QTableWidgetItem("Ждем..."));
+////        ui->tableWidget->setItemDelegateForColumn(1, new ProgressBarDelegate);
+//        fuelNameList(listConnections.at(i));
+//    }
+//    ui->tableWidget->horizontalHeader()->setStretchLastSection(true);
+    static int colTerm = m_listTerm.size();
+    ui->progressBar->setRange(0,colTerm);
+    ui->progressBar->setFormat("Получено %v из %m");
+    int progress = 0;
+    QSqlQuery q;
+    for(int i=0;i<colTerm;++i){
+            q.prepare("select c.TERMINAL_ID, c.SERVER_NAME, c.DB_NAME, c.CON_PASSWORD from CONNECTIONS c "
+                      "where c.TERMINAL_ID=:terminalID and c.CONNECT_ID=2");
+            q.bindValue(":terminalID", m_listTerm.at(i));
+            if(!q.exec()) qCritical(logCritical()) << "Не возможно получить данные о подключении АЗС" << q.lastError().text();
+            q.next();
+            QStringList list;
+            list << q.value(0).toString() << q.value(1).toString() << q.value(2).toString()  << passConv(q.value(3).toString());
+            listConnections.append(list);
+            QApplication::processEvents();
+            ++progress;
+            ui->progressBar->setValue(progress);
     }
-    ui->tableWidget->horizontalHeader()->setStretchLastSection(true);
+
 
 }
 
